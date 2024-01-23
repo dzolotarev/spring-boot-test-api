e6712da53cf27cda87cbba6ad26822bef0e0a0d7
Denis Zolotarev <judaeus@gmail.com>
2024-01-23 21:29:31 +0200

    * init project

---
+++ src/main/java/ru/dzolotarev/springbootapi/SpringBootApi.java
@@ -0,0 +1,13 @@
+package ru.dzolotarev.springbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApi {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApi.class, args);
+    }
+
+}
